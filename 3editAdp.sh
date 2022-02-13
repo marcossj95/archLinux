17925db22040629d43f27b3a5974e4d25fb384ec
Marcos <79965497+marcossj95@users.noreply.github.com>
2022-02-13 19:56:15 +0100

    Delete 3editAdp.sh

--- 3editAdp.sh
+++
@@ -1,7 +0,0 @@
-su
-systemctl start NetworkManager.service
-systemctl enable NetworkManager.service
-ip link
-ip link set edit up
-nmcli dev wifi connect MIFI_2G_kj7J password sfV6mxne
-pacman -S xorg-server xorg-xinit sddm xf86-video-nouveau plasma
